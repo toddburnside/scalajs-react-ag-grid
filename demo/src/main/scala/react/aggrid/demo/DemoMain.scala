b50c550f36af70f30f63892bbea77339f0f61ae7
Todd Burnside <tburnside@gemini.edu>
2020-12-21 07:59:37 -0800

    Test custom editor

--- demo/src/main/scala/react/aggrid/demo/DemoMain.scala
+++ demo/src/main/scala/react/aggrid/demo/DemoMain.scala
@@ -8,9 +8,32 @@ import js.annotation._
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import reactST.agGridReact.components._
+import reactST.agGridCommunity.iCellRendererMod.ICellRendererParams
+import reactST.agGridCommunity.iCellEditorMod.ICellEditorParams
 
 @JSExportTopLevel("DemoMain")
 object DemoMain {
+
+  def renderFn(props: ICellRendererParams) = renderer(
+    props.value.asInstanceOf[String]
+  ).raw
+
+  def editFn(props: ICellEditorParams) = Editor.component(props.value.asInstanceOf[String]).raw
+
+  val renderer = ScalaComponent
+    .builder[String]
+    .render_P((props: String) => <.span(s"I need a: ${props}"))
+    // .render_P((props: String) => <.span(props))
+    .build
+
+  val jsRenderer =
+    renderer
+      .cmapCtorProps[ICellRendererParams](p => p.value.asInstanceOf[String] + "?")
+      .toJsComponent
+      .raw
+
+  dom.console.log(Editor.jsComponent.asInstanceOf[js.Any])
+  dom.console.log(Editor.component("XXX").raw.asInstanceOf[js.Any])
 
   trait RowData extends js.Object {
     val make: String
@@ -37,10 +60,22 @@ object DemoMain {
           ^.cls := "ag-theme-alpine",
           ^.height := "400px",
           ^.width := "600px",
-          AgGridReact.rowData(rowData)(AgGridColumn.ColDef.field("make"),
-                                       AgGridColumn.ColDef.field("model"),
-                                       AgGridColumn.ColDef.field("year")
-          )
+          AgGridReact
+            .disableStaticMarkup(
+              true
+            ) // necessary to keep cell renderers from duplicating in the first cell
+            .rowData(rowData)(
+              AgGridColumn.ColDef
+                .field("make"),
+              AgGridColumn.ColDef
+                .field("model")
+                .editable(true)
+                // .cellRendererFramework(renderFn _)
+                .cellRendererFramework(jsRenderer)
+                // .cellEditorFramework(editFn _),
+                .cellEditorFramework(Editor.jsComponent),
+              AgGridColumn.ColDef.field("year")
+            )
         )
       }
       .build
@@ -70,4 +105,30 @@ object DemoMain {
 
     ()
   }
+}
+
+@JSExportTopLevel("Editor")
+object Editor {
+
+  // @JSExportAll
+  class Backend() {
+    def render(p: String): VdomNode =
+      <.input(^.defaultValue := p, ^.width := "100%")
+
+    // val getValue: js.Function0[js.Any] = () => "Edited, eh?"
+
+    // @JSExport
+    def getValue(): js.Any = "Edited"
+
+    def isCancelBeforeStart() = { println("isCancelBeforeStart() called"); false }
+    def isCancelAfterEnd() = { println("isCancelAfterEnd called"); false }
+    def afterGuiAttached() = println("Gui Attached")
+  }
+
+  def fromProps(q: ICellEditorParams): String = q.value.asInstanceOf[String] + "!"
+
+  val component = ScalaComponent.builder[String].renderBackend[Backend].build
+
+  // @JSExport
+  val jsComponent = component.cmapCtorProps[ICellEditorParams](fromProps).toJsComponent.raw
 }
